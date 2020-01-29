8825103a50127f774a2c6ed2c8d32bca7b39a79b
OJIEG65 <OJIEG65@gmail.com>
2020-01-29 15:58:38 +0200

    add get menuItem api

--- src/modules/menuItem/controller.js
+++ src/modules/menuItem/controller.js
@@ -1,17 +1,38 @@
-import MenuItem from "./models/MenuItem.js";
+import MenuItemModel from "./models/MenuItem.js";
+import MenuModel from "../menu/models/Menu.js";
 
 export const createMenuItem = async (req, res) => {
     const {name, price, description, menuId} = req.body;
     if (!name || !price || !description || !menuId)
         throw new Error('name, price, description and menuId are required fields');
     try {
-        const menuItem = await MenuItem.create({
+        const menuItem = await MenuItemModel.create({
             name,
             price,
             description,
             menuId: menuId
         });
         res.status(201).json(menuItem);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send('Something went wrong');
+    }
+};
+
+export const getMenuItem = async (req,res) => {
+    const {menuId, restaurantId} = req.query;
+    try {
+        if (restaurantId) {
+            const menusInRestaurant = await MenuModel.find({restaurant:restaurantId});
+            const menuIds = menusInRestaurant.map(item => item._id);
+            const items = await MenuItemModel.find({menuId: {$in: menuIds}});
+            res.status(200).send(items);
+        } else {
+            const items = await MenuItemModel.find({
+                menuId
+            });
+            res.status(200).json(items)
+        }
     } catch (e) {
         console.log(e);
         res.status(500).send('Something went wrong');
--- src/modules/menuItem/router.js
+++ src/modules/menuItem/router.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import {runAsyncWrapper} from "../../utils/asyncMiddleware.js";
-import {createMenuItem} from "./controller.js";
+import {createMenuItem, getMenuItem} from "./controller.js";
 
 const menuItemRouter = new express.Router();
 
 menuItemRouter.post('/', runAsyncWrapper(createMenuItem));
-// menuRouter.put('/:id', runAsyncWrapper());
-// menuRouter.get('/', runAsyncWrapper());
-// menuRouter.delete('/:id', runAsyncWrapper());
+// menuItemRouter.put('/:id', runAsyncWrapper());
+menuItemRouter.get('/', runAsyncWrapper(getMenuItem));
+// menuItemRouter.delete('/:id', runAsyncWrapper());
 
 export default menuItemRouter
