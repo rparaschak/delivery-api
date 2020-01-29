f4d0f7d2a18223d9d268c53cf306d53901e7b920
OJIEG65 <OJIEG65@gmail.com>
2020-01-29 10:39:56 +0200

    connect DB to app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 import menuRouter from './modules/menu/router.js';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ type: 'application/*+json' }));
+// const userPass = 'nH1MzZgaSB5yrdN1';
+// const userLogin = 'deliverUser';
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({type: 'application/*+json'}));
 app.use(bodyParser.json());
 
 app.use(menuRouter);
 
+mongoose.connect('mongodb+srv://deliverUser:nH1MzZgaSB5yrdN1@deliver-kxvhj.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
+  .then(() => {
+    console.log('COnnected to DB');
+    app.listen(3000, () => console.log(`Example app listening on port 3000!`));
+  })
+  .catch(() => { console.log('Could not connect to DB'); });
 
-app.listen(3000, () => console.log(`Example app listening on port 3000!`));
