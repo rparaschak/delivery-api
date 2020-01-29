414fcc2473384bb6c25d568407f17f1c9926f9df
OJIEG65 <OJIEG65@gmail.com>
2020-01-29 11:48:10 +0200

    add delete meny api

--- src/modules/menu/controller.js
+++ src/modules/menu/controller.js
@@ -1,4 +1,5 @@
 import MenuModel from './models/Menu.js';
+import mongoose from 'mongoose';
 
 export const createMenu = async (req, res) => {
     const {name} = req.body;
@@ -11,6 +12,23 @@ export const createMenu = async (req, res) => {
         });
 
         res.status(201).json(createdMenu);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send('Something went wrong');
+    }
+};
+
+
+export const deleteMenu = async (req, res) => {
+    const {id} = req.body;
+    if (!id)
+        throw new Error('Id field is required');
+    try {
+        const _id = mongoose.Types.ObjectId(id);
+        await MenuModel.deleteOne({
+            _id
+        });
+        res.status(201).json('menu deleted successfully');
     } catch (e) {
         console.log(e);
         res.status(500).send('Something went wrong');
