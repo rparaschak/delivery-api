b470d3e42d950c585af160310e73e3ddaa6b8552
rparashchak <roman.paraschak@gmail.com>
2020-01-29 10:21:25 +0200

    boilerplates

---
+++ src/modules/menu/models/Menu.js
@@ -0,0 +1,5 @@
+import mongoose, {Schema} from 'mongoose';
+
+const MenuSchema = new Schema({
+    name:  String,
+});
---
+++ src/modules/menu/router.js
@@ -0,0 +1,5 @@
+import {Router} from 'express';
+
+const menuRouter = new Router();
+
+menuRouter.get();
